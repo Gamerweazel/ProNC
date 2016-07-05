29fc789622fdf375dc0dfe29e825a3868a964311
Andrew Eames <aeames@terbium.cs.dixie.edu>
2016-07-05 15:29:16 -0600

    setup express to serve index.html

--- ReactTodoApp/app.js
+++ ReactTodoApp/app.js
@@ -3,16 +3,25 @@ var mongoose = require('mongoose')
 var morgan = require('morgan')
 var passport = require('passport')
 var skipper = require('skipper')
-
+var colors = require("colors")
+var path = require("path")
 // Configure Mongoose
 
-mongoose.connect('mongodb://localhost/ReactTodoApp')
-
+mongoose.connect('mongodb://mongodb.cs.dixie.edu/SudoBashBash')
+mongoose.connection.on("connected", function() {
+	console.log("Connected to DB...".green);
+})
+mongoose.connection.on("error", function(err) {
+	console.log("Connected to DB Failed...".red + err.message);
+})
 // require('./config/passport')
 
 // Configure Express
 
 var app = express()
+
+// setup statics
+app.use(express.static(__dirname + "/dist"))
 
 app.use(morgan('dev'))
 // app.use(passport.initialize())
@@ -23,10 +32,10 @@ app.use('/users', require('./server/user/routes'))
 
 app.all('/', function (req, res)
 {
-	res.end('You have made it to day 2!')
+	res.sendFile(path.join(__dirname, "/dist/index.html"));
 })
 
 app.listen(9005, function ()
 {
-	console.log('Listening on localhost:9005')
+	console.log('Listening on localhost:9005'.blue)
 })
